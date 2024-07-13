2757b39a1b8512728f08cebeb3b9945fc280a11c
Koval Yaroslav <yaroslav@koval.page>
2024-07-13 12:12:54 +0200

    Update BackButtonTelegram

--- src/components/BackButtonTelegram.ts
+++ src/components/BackButtonTelegram.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next-nprogress-bar";
 
 import { initBackButton } from '@tma.js/sdk-react';
@@ -5,10 +6,24 @@ import { initBackButton } from '@tma.js/sdk-react';
 
 const BackButtonTelegram = () => {
     const router = useRouter();
+    const [canGoBack, setCanGoBack] = useState<boolean>(false);
     const [backButton] = initBackButton();
 
-    backButton.show();
-    backButton.on('click', () => router.back());
+    useEffect(() => {
+        if (window.history.length > 1) 
+            setCanGoBack(true);
+    }, []);
+
+    useEffect(() => {
+        if (canGoBack) backButton.show();
+        else backButton.hide();
+    }, [canGoBack])
+
+    const handleBack = () => {
+        if (canGoBack) router.back();
+    };
+
+    backButton.on('click', () => handleBack());
 
     return null;
 };
